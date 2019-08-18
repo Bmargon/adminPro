db6bc180cf3563d81fbe425256ecf4b084b2e0ca
Bruno Martínez <bruno132mg98@gmail.com>
2019-08-18 17:29:53 +0200

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// ruta
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { NoPageFoundComponent } from './shared/no-page-found/no-page-found.component';
+import { DashBoardComponent } from './pages/dash-board/dash-board.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadCrumsComponent } from './shared/bread-crums/bread-crums.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './log-in/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    NoPageFoundComponent,
+    DashBoardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadCrumsComponent,
+    PagesComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
