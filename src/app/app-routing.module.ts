9949b9f325f235e53f916e032ab768723d159c6d
Bruno Martínez <bruno132mg98@gmail.com>
2019-08-18 21:19:12 +0200

    Modificaciones en los modulos y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DashBoardComponent } from './pages/dash-board/dash-board.component';
+
+
 import { LogInComponent } from './log-in/log-in.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NoPageFoundComponent } from './shared/no-page-found/no-page-found.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './log-in/register.component';
 
 
 const routes: Routes = [
-
-  { path: '', component: PagesComponent, children: [
-      { path: 'dashboard', component: DashBoardComponent },
-      { path: 'progress', component: ProgressComponent},
-      { path: 'graficas1', component: Graficas1Component},
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-
-    ]},
-
   { path: 'login', component: LogInComponent},
   { path: 'register', component: RegisterComponent},
-
   { path: '**', component: NoPageFoundComponent}
-
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,36 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 // ruta
 import { AppRoutingModule } from './app-routing.module';
-
-
+// modules
+import { PagesModule } from './pages/pages.module';
+// components
 import { AppComponent } from './app.component';
 import { LogInComponent } from './log-in/log-in.component';
-import { NoPageFoundComponent } from './shared/no-page-found/no-page-found.component';
-import { DashBoardComponent } from './pages/dash-board/dash-board.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadCrumsComponent } from './shared/bread-crums/bread-crums.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './log-in/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LogInComponent,
-    NoPageFoundComponent,
-    DashBoardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadCrumsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
+    PagesModule,
     AppRoutingModule
   ],
   providers: [],
